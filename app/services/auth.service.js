8506c449219f35a25a5f51a8aecef09248caec8e
ankit.sendhav <ankit.sendhav@hiddenbrains.in>
2022-06-08 00:06:55 +0530

    login api done

--- app/services/auth.service.js
+++ app/services/auth.service.js
@@ -7,15 +7,37 @@ class AuthService {
     return db('customer').insert(data);
   };
 
-  login = (data) => {
-    return db('user').select().where('userEmail', data);
+  getPhoneNumber = (data) => {
+    return db('customer').select().where('vPhonenumber', data);
   };
-  getpassword = (data) => {
-    return db('user').select().where('userEmail', data);
+  getEmail = (data) => {
+    console.log(data);
+    return db('customer').select().where('vEmail', data);
   };
+  getPasswordByEmail = (data) => {
+    return db('customer').select().where('vEmail', data);
+  };
+  getPasswordByPhone = (data) => {
+    return db('customer').select().where('vPhonenumber', data);
+  };
+
+  loginWithEmail = (data) => {
+    return db('customer').select().where('vEmail', data);
+  };
+  loginWithPhone = (data) => {
+    return db('customer').select().where('vPhonenumber', data);
+  };
+
+  updateAccessKey = (id, key) => {
+    return db('customer').where('iCustomerId', '=', id).update({
+      vAccessKey: key,
+    });
+  };
+
   getUser = (data) => {
     return db('user').select().where('id', data);
   };
+
   forgotPassword = (data) => {
     return db('user').select().where('userEmail', data);
   };
