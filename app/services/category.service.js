5db2973a265fa24faa64fa8272de0cf809bf47b3
ankit.sendhav <ankit.sendhav@hiddenbrains.in>
2022-06-29 22:43:34 +0530

    category list completed

--- app/services/category.service.js
+++ app/services/category.service.js
@@ -1,13 +1,21 @@
+import { query } from 'express';
 import db from '../config/database';
 
 class CategoryService {
   constructor() {}
 
   getCategoryList = (data) => {
-    return db('category')
-      .select('*')
-      .offset(data.limit * (data.page_number - 1))
-      .limit(data.limit);
+    let query = db('category').select('*');
+
+    if (data && data.keyword) {
+      query
+        .where('vCategoryName', 'like', `%${data.keyword}%`)
+        .offset(data.limit * (data.page_number - 1))
+        .limit(data.limit);
+    } else {
+      query.offset(data.limit * (data.page_number - 1)).limit(data.limit);
+    }
+    return query;
   };
   getCategoryCounts = () => {
     return db('category').count();
